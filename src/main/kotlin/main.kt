33976743348e28b401efaed3b66de2f46094e4b7
Alejandro Cruz <alejandro.cruz@outlook.com>
2021-07-17 13:49:21 -0700

    Update print function

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,9 @@
 import kotlin.math.sqrt
 
-fun main(args: Array<String>) {
-    val sudoku = arrayOf(
+fun main() {
+    val sudokuFirst = arrayOf(
         mutableListOf(5, 0, 0, 9, 1, 3, 7, 2, 0),
-        mutableListOf(3, 0, 0, 9, 8, 3, 5, 0, 9),
+        mutableListOf(3, 0, 0, 0, 8, 0, 5, 0, 9),
         mutableListOf(0, 9, 0, 2, 5, 0, 0, 8, 0),
         mutableListOf(6, 8, 0, 4, 7, 0, 2, 3, 0),
         mutableListOf(0, 0, 9, 5, 0, 0, 4, 6, 0),
@@ -13,9 +13,27 @@ fun main(args: Array<String>) {
         mutableListOf(8, 5, 0, 7, 2, 0, 0, 0, 3)
     )
 
-    solveSudoku(sudoku)
+    solveSudoku(sudokuFirst)
 
-    sudoku.forEach { row -> println(row.toIntArray().contentToString()) }
+    printSudoku(sudokuFirst)
+
+    println("==========================================================")
+
+    val sudokuSecond = arrayOf(
+        mutableListOf(6, 9, 0, 0, 0, 0, 7, 0, 0),
+        mutableListOf(0, 0, 0, 0, 9, 6, 0, 0, 0),
+        mutableListOf(0, 8, 0, 7, 5, 3, 0, 9, 0),
+        mutableListOf(0, 2, 0, 3, 7, 4, 5, 6, 1),
+        mutableListOf(3, 6, 0, 0, 0, 5, 0, 2, 0),
+        mutableListOf(0, 0, 0, 9, 6, 0, 3, 7, 8),
+        mutableListOf(0, 0, 6, 0, 3, 1, 0, 8, 4),
+        mutableListOf(0, 4, 5, 8, 0, 7, 6, 0, 0),
+        mutableListOf(0, 0, 0, 0, 0, 0, 0, 5, 7)
+    )
+
+    solveSudoku(sudokuSecond)
+
+    printSudoku(sudokuSecond)
 }
 
 fun isAvailable(sudoku: Array<MutableList<Int>>, row: Int, col: Int, number: Int): Boolean {
@@ -45,7 +63,7 @@ fun isAvailable(sudoku: Array<MutableList<Int>>, row: Int, col: Int, number: Int
 }
 
 fun solveSudoku(sudoku: Array<MutableList<Int>>): Boolean {
-    var isEmpty = true;
+    var isEmpty = true
 
     sudoku.forEachIndexed { row, list ->
         list.forEachIndexed { col, item ->
@@ -70,4 +88,21 @@ fun solveSudoku(sudoku: Array<MutableList<Int>>): Boolean {
         return true
 
     return false
+}
+
+fun printSudoku(sudoku: Array<MutableList<Int>>) {
+    println("╔═══════════╦═══════════╦═══════════╗")
+    for (i in sudoku.indices) {
+        if (i != 0 && i.mod(3) == 0)
+            println("╠═══════════╬═══════════╬═══════════╣")
+        sudoku[i].forEachIndexed { index, num ->
+            if (index.mod(3) == 0)
+                print("║")
+            else
+                print("│")
+            print(" $num ")
+        }
+        println("║")
+    }
+    println("╚═══════════╩═══════════╩═══════════╝")
 }
